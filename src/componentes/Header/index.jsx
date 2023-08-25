5cc40259a2697a52ef162f1d27d03fa6787c242e
Nicolás <nick.mdz.b@gmail.com>
2023-08-25 10:55:42 -0600

    Inicializando proyecto

---
+++ src/componentes/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header () {
+    return <header>
+        <div className="Header">
+            <img src="/img/LogoMain.png" alt="Logo Aluraflix" />
+            <button className="btn-header">Nuevo Video</button>
+        </div>
+        
+    </header>
+}
+
+export default Header
