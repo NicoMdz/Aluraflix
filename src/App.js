fecf076e3aadee6eb34ac8bbaabb3ee15f5c4c3c
Nicolás Méndez Brito <nick.mdz.b@gmail.com>
2023-08-28 16:42:43 -0600

    Atcualizando cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Header from "./componentes/Header";
 import Banner from "./componentes/Banner";
 import Footer from "./componentes/Footer";
+import DefaultPage from "./componentes/DefaultPage";
+import MultipleItems from "./componentes/HomeCarousel/Carousel"
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Banner />
+        <MultipleItems />
         <Footer />
+        {/* <DefaultPage /> */}
     </div>
   );
 }
---
+++ src/componentes/DefaultPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+const DefaultPage = (props) => {
+    <>
+    <Header />
+
+    <Footer />
+    </>
+    
+}
+
+export default DefaultPage
